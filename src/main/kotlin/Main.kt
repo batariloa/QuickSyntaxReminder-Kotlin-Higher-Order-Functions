0cd4fcd519ae8a312a687e1d72923e18116359bb
Andrej Batarilo <batariloa@icloud.com>
2022-04-15 19:24:15 +0200

    Second commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,22 +3,38 @@ import kotlin.concurrent.thread
 fun main() {
 
     val addition = doAddition()
-   println(addition(2,3))
+    println(addition(2,3))
 
-
+    //passing a function that prints the result
     rollDice { println(it) }
 
-    rollDice() //null
+    //passing a function to function
+    val broj = rollDicePassFunction(){ generateNumberForDice() } // store a function call in a variable
+
+    println(broj())  //call the function stored
     }
 
 
 fun addNumbers(x:Int,y:Int) = x.plus(y)
+
+
 
 fun doAddition():(Int,Int) ->Int{
 
     return ::addNumbers
 }
 
+
+//generate a number from 1 to 6
+fun generateNumberForDice():Int { return (1..6).random(); }
+
+//accepts a function without parameters that returns Int 
+fun rollDicePassFunction(callback: () -> Int): () -> Int {
+
+    return callback
+}
+
+//accepts a function without parameters, that returns nothing. This function is optional.
 fun rollDice(callback: ((result:Int) -> Unit)?=null ):String{
 
     thread{
